31006dc12d76c984f42e49da55f208f84a12ac56
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-07-01 20:03:08 -0400

    Added

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+const Contact = () => {
+  return (
+    <Layout title="KrishnaKanth Kondoju | Front-End developer">
+      <div>tets</div>
+    </Layout>
+  );
+};
+
+export default Contact;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
+import Loader from "../components/loader";
 
 // window.onload = () => {
 //   var script = document.createElement("script");
@@ -49,200 +50,6 @@ function Feature({ imageUrl, title, description }) {
   );
 }
 
-function Loading() {
-  return (
-    <div className={styles.socket}>
-      <div className={clsx(styles.gel, styles.centergel)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c1, styles.r1)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c2, styles.r1)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c3, styles.r1)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c4, styles.r1)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c5, styles.r1)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c6, styles.r1)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-
-      <div className={clsx(styles.gel, styles.c7, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-
-      <div className={clsx(styles.gel, styles.c8, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c9, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c10, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c11, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c12, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c13, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c14, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c15, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c16, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c17, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c18, styles.r2)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c19, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c20, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c21, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c22, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c23, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c24, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c25, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c26, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c28, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c29, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c30, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c31, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c32, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c33, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c34, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c35, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c36, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-      <div className={clsx(styles.gel, styles.c37, styles.r3)}>
-        <div className={clsx(styles.hexbrick, styles.h1)}></div>
-        <div className={clsx(styles.hexbrick, styles.h2)}></div>
-        <div className={clsx(styles.hexbrick, styles.h3)}></div>
-      </div>
-    </div>
-  );
-}
-
 function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
@@ -253,12 +60,9 @@ function Home() {
   }, []);
 
   return (
-    <Layout
-      title="KrishnaKanth Kondoju | Front-End developer"
-      // description="KrishnaKanth Kondoju | Front-End developer"
-    >
+    <Layout title="KrishnaKanth Kondoju | Front-End developer">
       {loader ? (
-        <Loading />
+        <Loader />
       ) : (
         <>
           <header className={clsx("hero hero--primary", styles.heroBanner)}>
@@ -275,7 +79,7 @@ function Home() {
                   )}
                   to={useBaseUrl("docs/")}
                 >
-                  Get Started
+                  Hire me
                 </Link>
               </div>
             </div>
